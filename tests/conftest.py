1711921684d8c9ad2e628d981d4767ac8ada6a49
Prayson Wilfred Daniel <prayson.wilfred@nttdata.com>
2023-01-05 17:29:03 +0100

    Update conftest.py
    Added fields

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,15 @@ from .types import Artifacts
 
 @pytest.fixture()
 def artifacts() -> Generator[Artifacts, None, None]:
-    yield Artifacts()
+    yield Artifacts(bigram_mapper={
+        ("new", "york"): "new_york",
+        ("baby", "again!"): "baby_again!",
+    },
+    in_sentences=[
+        ["this", "is", "new", "york", "baby", "again!"],
+        ["new", "york", "and", "baby", "again!"],
+    ],
+    out_senteces=[
+        ["this", "is", "new_york", "baby_again!"],
+        ["new_york", "and", "baby_again!"],
+    ],)
