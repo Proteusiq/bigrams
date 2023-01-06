a4e30ea1a0747bfeb59bb2aff5ebc2c6a5f24dd8
Prayson W. Daniel <prayson.wilfred@nttdata.com>
2023-01-06 08:07:34 +0100

    precommit fix

--- tests/types.py
+++ tests/types.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Artifacts:
-    bigram_mapper: dict[tuple[str,str],str]
+    bigram_mapper: dict[tuple[str, str], str]
     in_sentences: list[list[str]]
     out_senteces: list[list[str]]
